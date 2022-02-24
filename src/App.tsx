a648ee6290fbde70a6ef0bfcd71d4caac428eb2a
Duc Vo <pianote@gmail.com>
2022-02-24 12:30:02 -0800

    include github and linkedin link to frontpage

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,37 @@ function App() {
 		<div
 			style={{
 				display: 'flex',
+				flexDirection: 'column',
 				justifyContent: 'center',
 				alignItems: 'center',
-        height: '100vh',
-        color: 'salmon',
-        textTransform: 'uppercase',
-        fontFamily: 'futura',
+				height: '100vh',
+				color: 'salmon',
+				textTransform: 'uppercase',
+				fontFamily: 'futura'
 			}}
 		>
-			My Portfolio
+			<div>My Portfolio</div>
+			<div style={{fontSize: '0.7rem'}}>
+				<a
+					href="https://github.com/pianote/"
+					style={{
+						textDecoration: 'none',
+						color: 'salmon'
+					}}
+				>
+					github
+				</a>{' '}
+				|{' '}
+				<a
+					href="https://www.linkedin.com/in/ducmvo/"
+					style={{
+						textDecoration: 'none',
+						color: 'salmon'
+					}}
+				>
+					linkedin
+				</a>
+			</div>
 		</div>
 	)
 }
